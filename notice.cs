5075e57f084a2eb3127fb76d3774295e1b88e785
Mutasim <95741325+meowtasim@users.noreply.github.com>
2023-02-25 20:02:00 +0530

    Add files via upload

---
+++ notice.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class notice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene(16);
+    }
+}
