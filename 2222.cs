e64fed6f1bc395e5b9a5249713493aa935f5ea92
Mutasim <95741325+meowtasim@users.noreply.github.com>
2023-02-26 08:49:31 +0530

    Add files via upload

---
+++ 2222.cs
@@ -0,0 +1,14 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class 2222 : MonoBehaviour
+//{
+//    public string name;
+//    public int gold;
+//    public User(string name, int gold)
+//    {
+//        this.name = name;
+//        this.gold = gold;
+//    }
+//}
